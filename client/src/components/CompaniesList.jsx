9d43e972b6c2cb9cfcf7ced134a5026c8079ac58
nintichance <mintidance@gmail.com>
2018-03-03 19:14:18 -0500

    Added some more styling to companies

--- client/src/components/CompaniesList.jsx
+++ client/src/components/CompaniesList.jsx
@@ -1,23 +1,30 @@
 import React, { Component } from 'react'
 import CompaniesIndex from './CompaniesIndex'
-import { CompanyIndexContainer } from './styled-components/Containers'
+import { CompanyIndexContainer, SplashImageContainer, SplashHeaderContainer } from './styled-components/Containers'
 import { SplashImage } from './styled-components/Images'
 
+
 const CompaniesList = (props) => {
-    const companiesList = props.companies.map((company, index)=>{
-        return(
+    const companiesList = props.companies.map((company, index) => {
+        return (
             <div>
-                
+
                 <CompaniesIndex />
             </div>
         )
     })
 
-    return(
-        <CompanyIndexContainer>
-            <SplashImage src="https://i.imgur.com/CMSTGj4.jpg?1"/>
-        {companiesList}
-        </CompanyIndexContainer>
+    return (
+        <div>
+             <SplashImageContainer>
+                <SplashImage src="https://i.imgur.com/CMSTGj4.jpg?1" />
+                <SplashHeaderContainer>Organizations</SplashHeaderContainer>
+            </SplashImageContainer>
+            <CompanyIndexContainer>
+                        {companiesList}
+            </CompanyIndexContainer>
+        </div>
+     
     )
 }
 
--- client/src/components/styled-components/Containers.jsx
+++ client/src/components/styled-components/Containers.jsx
@@ -36,9 +36,47 @@ export const CompanyIndexContainer = styled.div `
     flex-wrap: wrap;
     justify-content: space-around;
 `
+export const SplashHeaderContainer = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    
+`
 
+export const SplashImageContainer = styled.div`
+    font-size: 10em;
+    position: relative;
+    text-align: center;
+    color: white;
+    color: white;
+    font-family: 'Raleway', sans-serif;
+    text-shadow: 1px 1px rgba(0,0,0, 0.50);  
+    @media only screen and (max-width: 787px){
+        font-size: 5em
+    
+    }
+    @media only screen and (max-width: 600px){
+        font-size: 3em
+    
+    }
+    @media only screen and (max-width: 400px){
+        font-size: 2em
+    
+    }
+    @media only screen and (max-width: 350px){
+        font-size: 1.5em
+    
+    }
+    @media only screen and (max-width: 300px){
+        font-size: 1em
+    
+    }
+`
 
 export default { 
+    SplashImageContainer,
+    SplashHeaderContainer,
     CompanyIndexContainer,
     CompanyHeaderContainer, 
     CompanyContainer
