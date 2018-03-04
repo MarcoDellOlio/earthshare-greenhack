7bf7eef5968d6579582e54b9e8bde8b94d09db63
nintichance <mintidance@gmail.com>
2018-03-04 11:44:21 -0500

    Styling Home Page

--- client/src/components/styled-components/NavStyle.jsx
+++ client/src/components/styled-components/NavStyle.jsx
@@ -1,31 +1,12 @@
 import styled from 'styled-components'
 
+
 export const NavDiv = styled.div`
-width: 100vw;
-// position: fixed;  
-color: #f9f9f9;
-top: 0;
-left: 0;
-display: flex;
-justify-content: space-between;
-align-items: center;
-height: 10vh;
-margin-bottom: 25px;
-background-color: #2FAE1C;
-box-shadow: 0 2px 0 0 rgba(0, 0, 0, 0.1);
-z-index: 1;
-a{
-    text-decoration: none;
-    color: #f9f9f9;
-    margin: 10px;
-    margin-right: 20px;
-    font-size: 1.25em;
-    text-shadow: 1px 1px rgba(0,0,0, 0.1);
-}
+    display: flex;
+    justify-content: space-around;
+    background-color: rgba(99, 160, 200, 0.1);
+
 `
-
-
-
 
 export default {
    NavDiv
